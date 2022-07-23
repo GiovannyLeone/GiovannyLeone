1e7a6f35b85788253e900ea447ea1a1bc752364f
GiovannyLeone <giovanny.leone.barbosa@gmail.com>
2022-07-23 15:51:14 -0300

    Update the second version of my Portfolio, animations with sass and functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import './assets/scss/animation.scss';
 import Profile from './components/Profile'
 
 function App() {
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,25 +1,60 @@
 import React from 'react'
 import Me from '../assets/img/me.png'
+import $ from 'jquery'
+
+
+
+
 
 export default function Profile() {
 
+
+    function teste() {
+        alert("!!!")
+        return false
+    }
+
+
     return (
         <>
+        <div>
+                <section>
+                    <span className="starts"></span>
+                    <span className="starts"></span>
+                    <span className="starts"></span>
+                    <span className="starts"></span>
+                    <span className="starts"></span>
+                    <span className="starts"></span>
+                    <span className="starts"></span>
+                    <span className="starts"></span>
+                    <span className="starts"></span>
+                    <span className="starts"></span>
+                </section>
+            </div>
             <div id='profile'>
                 <div id='myPicture'>
-                    <img src={Me} alt="Esse sou eu" className='me' />
+                    <img src={Me} alt="Esse sou eu" className='me animated zoom delay-1s' />
                 </div>
                 <div id='aboutMe'>
-                    <h1 className='myName'>Giovanny Leone</h1>
-                    <p className='textAboutMe'>Tenho 19 anos sou estudante de ensino técnico, desenvolvo aplicações web tanto Front-End
+                    <figure>
+                        <svg viewBox="0 0 1320 300">
+                            <text className="xl g" x="17%" y="65%" dy=".35em" text-anchor="middle">Giovanny</text>
+                            <text className="xl l" x="25%" y="90%" dy=".35em" text-anchor="middle">Leone</text>
+                            <text className="xl dev" x="18.5%" y="110%" dy=".35em" text-anchor="middle">Dev Full-Stack Junior</text>
+                        </svg>
+                    </figure>
+                    <p className='textAboutMe'>Olá Mundo! Me chamo Giovanny, Tenho 19 anos sou estudante de ensino técnico, desenvolvo aplicações web tanto Front-End
                         como Back-End, focado sempre em aprender mais, possuo conhecimentos em HTML5, CSS3,
-                        JavaScript, PHP, Jquery, React.js, Next.js, typeScript, MySql, Sql Server e modelagem de
+                        JavaScript, PHP, Jquery, React, Next.js, TypeScript, MySql, Sql Server e modelagem de
                         banco dados.</p>
-                    <button>My Soft Skills</button>
+                    <button id='skills' onClick={()=>{teste()}}>My Soft Skills</button>
                     <button>Meet my Portfolio</button>
                 </div>
             </div>
+
+        
         </>
+
     )
 
 }
