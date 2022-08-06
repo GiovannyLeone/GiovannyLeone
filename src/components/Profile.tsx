aeb0dc55cf7fdc75ae0815987ce291c7f7334c7a
GiovannyLeone <giovanny.leone.barbosa@gmail.com>
2022-08-06 04:31:45 -0300

    Change the text about-me

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -40,12 +40,12 @@ export default function Profile() {
                         <svg viewBox="0 0 1320 300">
                             <text className="xl g" x="17%" y="65%" dy=".35em" text-anchor="middle">Giovanny</text>
                             <text className="xl l" x="25%" y="90%" dy=".35em" text-anchor="middle">Leone</text>
-                            <text className="xl dev" x="18.5%" y="110%" dy=".35em" text-anchor="middle">Full-Stack Dev Junior</text>
+                            <text className="xl dev" x="18.5%" y="110%" dy=".35em" text-anchor="middle">Full-Stack Dev</text>
                         </svg>
                     </figure>
-                    <p className='textAboutMe'>Olá Mundo! Me chamo Giovanny, Tenho 19 anos sou estudante de ensino técnico, desenvolvo aplicações web tanto Front-End
-                        como Back-End, focado sempre em aprender mais, possuo conhecimentos em HTML5, CSS3,
-                        JavaScript, PHP, Jquery, React, Next.js, TypeScript, MySql, Sql Server e modelagem de
+                    <p className='textAboutMe'>Olá Mundo! Me chamo Giovanny, tenho 19 anos e sou desenvolvedor junior. Desenvolvo aplicações web tanto Front-End
+                        como Back-End, focado sempre em aprender mais, e agregar conhecimento ao meu time, possuo conhecimentos em HTML5, CSS3,
+                        JavaScript, PHP, PDO, Jquery, React, Next.js, TypeScript, MySql, Sql Server e modelagem de
                         banco dados.</p>
                     <button id='skills' onClick={()=>{teste()}}>My Soft Skills</button>
                     <button>Meet my Portfolio</button>
