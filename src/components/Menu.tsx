0e806c34421addba075c2b8cd2b83d0f1d9ff069
GiovannyLeone <giovanny.leone.barbosa@gmail.com>
2022-08-08 01:38:17 -0300

    I messing with the Media query | responsive

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -9,11 +9,15 @@ import close from '../assets/img/menu/close.svg'
 
 
 export default function Menu() {
+    function comingSoon() {
+        alert("Em breve | Coming soon")
+        return false
+    }
 
 
     return (
         <>
-            <div className="menu">
+            <div className="menu" onClick={()=>{comingSoon()}}>
             {/* <img src={close} alt="" v-if="opened" />
             <img src={menu} alt="" v-if="!opened" /> */}
 
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Me from '../assets/img/me.png'
+import Me from '../assets/img/profile3.png'
 import $ from 'jquery'
 
 
@@ -9,50 +9,50 @@ import $ from 'jquery'
 export default function Profile() {
 
 
-    function teste() {
-        alert("!!!")
+    function comingSoon() {
+        alert("Em breve | Coming soon")
         return false
     }
 
 
     return (
         <>
-        <div>
-                <section>
-                    <span className="starts"></span>
-                    <span className="starts"></span>
-                    <span className="starts"></span>
-                    <span className="starts"></span>
-                    <span className="starts"></span>
-                    <span className="starts"></span>
-                    <span className="starts"></span>
-                    <span className="starts"></span>
-                    <span className="starts"></span>
-                    <span className="starts"></span>
-                </section>
-            </div>
-            <div id='profile'>
-                <div id='myPicture'>
-                    <img src={Me} alt="Esse sou eu" className='me animated zoom delay-1s' />
+            <div id='responsive'>
+                <div>
+                    <section>
+                        <span className="starts"></span>
+                        <span className="starts"></span>
+                        <span className="starts"></span>
+                        <span className="starts"></span>
+                        <span className="starts"></span>
+                        <span className="starts"></span>
+                        <span className="starts"></span>
+                        <span className="starts"></span>
+                        <span className="starts"></span>
+                        <span className="starts"></span>
+                    </section>
                 </div>
-                <div id='aboutMe'>
-                    <figure>
-                        <svg viewBox="0 0 1320 300">
-                            <text className="xl g" x="17%" y="65%" dy=".35em" text-anchor="middle">Giovanny</text>
-                            <text className="xl l" x="25%" y="90%" dy=".35em" text-anchor="middle">Leone</text>
-                            <text className="xl dev" x="18.5%" y="110%" dy=".35em" text-anchor="middle">Full-Stack Dev</text>
-                        </svg>
-                    </figure>
-                    <p className='textAboutMe'>Olá Mundo! Me chamo Giovanny, tenho 19 anos e sou desenvolvedor junior. Desenvolvo aplicações web tanto Front-End
-                        como Back-End, focado sempre em aprender mais, e agregar conhecimento ao meu time, possuo conhecimentos em HTML5, CSS3,
-                        JavaScript, PHP, PDO, Jquery, React, Next.js, TypeScript, MySql, Sql Server e modelagem de
-                        banco dados.</p>
-                    <button id='skills' onClick={()=>{teste()}}>My Soft Skills</button>
-                    <button>Meet my Portfolio</button>
+                <div id='profile'>
+                    <div id='myPicture'>
+                        <img src={Me} alt="Esse sou eu" className='me animated zoom delay-1s' />
+                    </div>
+                    <div id='aboutMe'>
+                        <figure>
+                            <svg viewBox="0 0 1320 300">
+                                <text className="xl g" x="17%" y="65%" dy=".35em" text-anchor="middle">Giovanny</text>
+                                <text className="xl l" x="25%" y="90%" dy=".35em" text-anchor="middle">Leone</text>
+                                <text className="xl dev" x="18.5%" y="110%" dy=".35em" text-anchor="middle">Full-Stack Dev</text>
+                            </svg>
+                        </figure>
+                        <p className='textAboutMe'>Olá Mundo! Me chamo Giovanny, tenho 19 anos e sou desenvolvedor junior. Desenvolvo aplicações web tanto Front-End
+                            como Back-End, focado sempre em aprender mais, e agregar conhecimento ao meu time, possuo conhecimentos em HTML5, CSS3,
+                            JavaScript, PHP, PDO, Jquery, React, Next.js, TypeScript, MySql, Sql Server e modelagem de banco dados.</p>
+                        <button id='skills' onClick={() => { comingSoon() }}>My Soft Skills</button>
+                        <button id='portfolio' onClick={() => { comingSoon() }}>Meet my Portfolio</button>
+                    </div>
                 </div>
-            </div>
 
-        
+            </div>
         </>
 
     )
